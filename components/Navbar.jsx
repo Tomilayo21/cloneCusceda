8e1f9f6d7652f55edb2364b55ce985a5f75898ea
Tomilayo21 <cusceda@yahoo.com>
2025-05-01 18:36:38 +0100

    Second commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,12 +11,16 @@ const Navbar = () => {
   const { isSeller, router, user } = useAppContext();
   const {openSignIn} = useClerk()
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+    <nav className="flex items-center justify-between px-3 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
       <Image
         className="cursor-pointer w-28 md:w-32"
         onClick={() => router.push('/')}
-        src={assets.logo}
+        // src={assets.logo}
+        // alt="logo"
+        src="/Maw.png"
         alt="logo"
+        width={100}
+        height={100}
       />
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">
