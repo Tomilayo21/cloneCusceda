7c7194c7ef985ea48659f3b4accd0adfafcdb023
Tomilayo21 <cusceda@yahoo.com>
2025-05-15 13:54:46 +0100

    second commit

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -1,3 +1,30 @@
+// // import React, { useState } from "react";
+// // import Navbar from "./Navbar";
+// // import Filter from "./Filter"; // Assuming you have a Filter component
+
+// // const Searchbar = () => {
+// //   const [searchQuery, setSearchQuery] = useState("");
+
+// //   // Function to update the search query
+// //   const handleSearch = (query) => {
+// //     setSearchQuery(query);
+// //   };
+
+// //   return (
+// //     <div>
+// //       <Navbar searchQuery={searchQuery} onSearch={handleSearch} />
+// //       <Filter searchQuery={searchQuery} />
+// //     </div>
+// //   );
+// // };
+
+// // export default Searchbar;
+
+
+
+
+
+
 // import React, { useState } from "react";
 // import Navbar from "./Navbar";
 // import Filter from "./Filter"; // Assuming you have a Filter component
@@ -7,11 +34,12 @@
 
 //   // Function to update the search query
 //   const handleSearch = (query) => {
-//     setSearchQuery(query);
+//     setSearchQuery(query);  // This updates the search query in the state
 //   };
 
 //   return (
 //     <div>
+//       {/* Pass the search query and the onSearch handler to Navbar */}
 //       <Navbar searchQuery={searchQuery} onSearch={handleSearch} />
 //       <Filter searchQuery={searchQuery} />
 //     </div>
@@ -19,31 +47,3 @@
 // };
 
 // export default Searchbar;
-
-
-
-
-
-
-import React, { useState } from "react";
-import Navbar from "./Navbar";
-import Filter from "./Filter"; // Assuming you have a Filter component
-
-const Searchbar = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  // Function to update the search query
-  const handleSearch = (query) => {
-    setSearchQuery(query);  // This updates the search query in the state
-  };
-
-  return (
-    <div>
-      {/* Pass the search query and the onSearch handler to Navbar */}
-      <Navbar searchQuery={searchQuery} onSearch={handleSearch} />
-      <Filter searchQuery={searchQuery} />
-    </div>
-  );
-};
-
-export default Searchbar;
