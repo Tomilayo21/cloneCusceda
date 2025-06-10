636b516f6ce6f07fc30c479ebb9b4f4f92f4c0cc
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-06-10 02:17:19 +0100

    Footer.jsx

--- components/admin/Footer.jsx
+++ components/admin/Footer.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+import { useTheme } from "@/context/ThemeContext";
+
 
 const Footer = () => {
+  const { theme } = useTheme();
+
+  // Select logo based on theme
+  const logoSrc = theme === "dark" ? "/cusceda_.png" : "/cusceda.png";
+
   return (
     <div className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-10">
       <div className="flex items-center gap-4">
-        <Image className="hidden md:block" src={assets.logo} alt="logo" />
+       <Image
+            src={logoSrc}
+            alt="Cusceda Logo"
+            width={128}
+            height={128}
+            className="w-28 md:w-32 cursor-pointer"
+            priority
+          />
         <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © greatstack.dev All Right Reserved.
+          Copyright 2025 © Cusceda All Rights Reserved.
         </p>
       </div>
       <div className="flex items-center gap-3">
