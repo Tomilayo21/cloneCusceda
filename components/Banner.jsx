859a083bab06f3544106a4de060cdd69c7a00af7
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-06-10 02:20:17 +0100

    Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,22 +4,33 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">
+    <div
+      className="
+        flex flex-col md:flex-row items-center justify-between 
+        md:pl-20 py-14 md:py-0 my-16 rounded-xl overflow-hidden 
+        bg-[#E6E9F2] dark:bg-transparent 
+        border border-transparent dark:border-gray-700
+      "
+    >
       <Image
         className="max-w-56"
         src={assets.jbl_soundbox_image}
         alt="jbl_soundbox_image"
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
-        <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
+        <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px] text-black dark:text-white">
           Game On: Experience Next-Level Action
         </h2>
-        <p className="max-w-[343px] font-medium text-gray-800/60">
+        <p className="max-w-[343px] font-medium text-gray-800/60 dark:text-gray-400">
           Immersive audio meets precise controls—your winning combo.
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
           Add to Cart
-          <Image className="group-hover:translate-x-1 transition" src={assets.arrow_icon_white} alt="arrow_icon_white" />
+          <Image
+            className="group-hover:translate-x-1 transition"
+            src={assets.arrow_icon_white}
+            alt="arrow_icon_white"
+          />
         </button>
       </div>
       <Image
