64714a8655c3de884c761b9eba73f5903afd14d6
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-06-10 01:50:26 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
@@ -16,10 +17,12 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
-          <Toaster />
-          <AppContextProvider>
-            {children}
-          </AppContextProvider>
+          <ThemeProvider>
+            <Toaster />
+            <AppContextProvider>
+              {children}
+            </AppContextProvider>
+          </ThemeProvider>
         </body>
       </html>
       </ClerkProvider>
