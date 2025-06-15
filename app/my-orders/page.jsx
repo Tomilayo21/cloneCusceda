80de15697cbf76e4572cf0bbf7190d1dc9700829
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-06-15 01:11:30 +0100

    page.jsx

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -1,276 +1,6 @@
-// 'use client';
-// import React, { useEffect, useState } from "react";
-// import { assets } from "@/assets/assets";
-// import Image from "next/image";
-// import { useAppContext } from "@/context/AppContext";
-// import Footer from "@/components/Footer";
-// import Navbar from "@/components/Navbar";
-// import Loading from "@/components/Loading";
-// import axios from "axios";
-// import toast from "react-hot-toast";
-// import { useRouter } from "next/navigation";
-// import { useClerk, useUser} from "@clerk/nextjs";
+"use client";
 
-
-// const MyOrders = () => {
-//   const { currency, getToken, user } = useAppContext();
-//   const { openSignIn } = useClerk();
-  
-
-//     const { isLoaded, isSignedIn } = useUser();
-//   const [orders, setOrders] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const router = useRouter();
-
-
-//   useEffect(() => {
-//     if (isLoaded && !isSignedIn) {
-//       router.push("/"); // go to homepage first
-
-//       // Delay opening sign-in modal to allow router.push to finish
-//       setTimeout(() => {
-//         openSignIn(); // open Clerk modal
-//       }, 100); // slight delay (100ms)
-//     } else if (isSignedIn) {
-//       fetchOrders();
-//     }
-//   }, [isLoaded, isSignedIn]);
-
-//   if (!isLoaded || !isSignedIn) {
-//     return <Loading />;
-//   }
-
-//   const fetchOrders = async () => {
-//     try {
-//       const token = await getToken();
-//       const { data } = await axios.get('/api/order/list', {
-//         headers: { Authorization: `Bearer ${token}` }
-//       });
-
-//       if (data.success) {
-//         setOrders(data.orders.reverse());
-//         setLoading(false);
-//       } else {
-//         toast.error(data.message);
-//       }
-//     } catch (error) {
-//       toast.error(error.message);
-//     }
-//   };
-
-//   const handleCancelOrder = async (orderId) => {
-//     try {
-//       const token = await getToken();
-//       const { data } = await axios.post(
-//         "/api/order/cancel",
-//         { orderId },
-//         { headers: { Authorization: `Bearer ${token}` } }
-//       );
-
-//       if (data.success) {
-//         toast.success("Order cancelled");
-//         fetchOrders();
-//       } else {
-//         toast.error(data.message);
-//       }
-//     } catch (error) {
-//       toast.error(error.message);
-//     }
-//   };
-
-//   const handleDeleteOrder = async (orderId) => {
-//     try {
-//       const token = await getToken();
-//       const { data } = await axios.post(
-//         "/api/order/delete",
-//         { orderId },
-//         { headers: { Authorization: `Bearer ${token}` } }
-//       );
-
-//       if (data.success) {
-//         toast.success("Order deleted");
-//         fetchOrders();
-//       } else {
-//         toast.error(data.message);
-//       }
-//     } catch (error) {
-//       toast.error(error.response?.data?.message || error.message);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (user) {
-//       fetchOrders();
-//     }
-//   }, [user]);
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="flex flex-col justify-between mt-16 px-6 md:px-16 lg:px-32 py-6 min-h-screen">
-//         <div className="space-y-5">
-//           <h2 className="text-lg font-medium mt-6">My Orders</h2>
-//           {loading ? (
-//             <Loading />
-//           ) : (
-//             <div className="max-w-5xl border-t border-gray-300 text-sm">
-//               {orders.map((order, index) => (
-//                 <div
-//                   key={index}
-//                   className="flex flex-col md:flex-row gap-5 justify-between p-5 border-b border-gray-300"
-//                 >
-//                   <div className="flex-1 flex gap-5 max-w-80">
-//                     <Image
-//                       className="max-w-16 max-h-16 object-cover"
-//                       src={assets.box_icon}
-//                       alt="box_icon"
-//                       width={64}
-//                       height={64}
-//                     />
-//                     <p className="flex flex-col gap-3">
-//                       <span className="font-medium text-base">
-//                         {order.items
-//                           .map((item) => item.product.name + ` x ${item.quantity}`)
-//                           .join(", ")}
-//                       </span>
-//                       <span>Items : {order.items.length}</span>
-//                     </p>
-//                   </div>
-//                   <div>
-//                     <p>
-//                       <span className="font-medium">{order.address.fullName}</span>
-//                       <br />
-//                       <span>{order.address.area}</span>
-//                       <br />
-//                       <span>{`${order.address.city}, ${order.address.state}`}</span>
-//                       <br />
-//                       <span>{order.address.phoneNumber}</span>
-//                     </p>
-//                   </div>
-//                   <p className="font-medium my-auto">{currency}{order.amount}</p>
-//                   <div>
-//                     <p className="flex flex-col">
-//                       <span>Method : COD</span>
-//                       <span>Date : {new Date(order.date).toLocaleDateString()}</span>
-//                       <span>Payment : Pending</span>
-//                       <span>Status: {order.status}</span>
-//                     </p>
-//                     {order.status !== "Delivered" && order.status !== "Cancelled" && (
-//                       <button
-//                         onClick={() => handleCancelOrder(order._id)}
-//                         className="mt-2 px-3 py-1 text-sm bg-red-500 text-white rounded"
-//                       >
-//                         Cancel Order
-//                       </button>
-//                     )}
-//                     {order.status === "Cancelled" && (
-//                       <button
-//                         onClick={() => handleDeleteOrder(order._id)}
-//                         className="mt-2 px-3 py-1 text-sm bg-gray-700 text-white rounded"
-//                       >
-//                         Delete Order
-//                       </button>
-//                     )}
-//                   </div>
-//                 </div>
-//               ))}
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default MyOrders;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'use client';
 import React, { useEffect, useState } from "react";
-import { assets } from "@/assets/assets";
 import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
 import Footer from "@/components/Footer";
@@ -280,6 +10,11 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useClerk, useUser } from "@clerk/nextjs";
+import { assets } from "@/assets/assets";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
 
 const MyOrders = () => {
   const { currency, getToken } = useAppContext();
@@ -288,6 +23,19 @@ const MyOrders = () => {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
+
+  const paymentMethods = [
+    { id: "stripe", label: "Stripe", fee: 0.029 },
+    { id: "paypal", label: "PayPal", fee: 0.034 },
+    { id: "apple", label: "Apple Pay", fee: 0.025 },
+    { id: "google", label: "Google Pay", fee: 0.025 },
+    { id: "amazon", label: "Amazon Pay", fee: 0.03 },
+    { id: "bank-transfer", label: "Direct Bank Transfer", fee: 0.015 },
+    { id: "crypto", label: "Cryptocurrency", fee: 0.02 },
+    { id: "mpesa", label: "M-Pesa", fee: 0.025 },
+    { id: "paytm", label: "Paytm", fee: 0.02 },
+    { id: "cash-on-delivery", label: "Cash on Delivery", fee: 0 },
+  ];
 
   useEffect(() => {
     if (!isLoaded) return;
@@ -313,14 +61,14 @@ const MyOrders = () => {
           toast.error(data.message);
         }
       } catch (error) {
-        toast.error(error.message);
+        toast.error(error.message || "Failed to fetch orders.");
       } finally {
         setLoading(false);
       }
     };
 
     fetchOrders();
-  }, [isLoaded, isSignedIn]);
+  }, [isLoaded, isSignedIn, getToken, openSignIn, router]);
 
   const handleCancelOrder = async (orderId) => {
     try {
@@ -340,11 +88,12 @@ const MyOrders = () => {
             order._id === orderId ? { ...order, status: "Cancelled" } : order
           )
         );
+        router.push("/my-orders"); // Optional refresh
       } else {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error(error.message);
+      toast.error(error.message || "Failed to cancel order.");
     }
   };
 
@@ -366,7 +115,9 @@ const MyOrders = () => {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error(error.response?.data?.message || error.message);
+      toast.error(
+        error.response?.data?.message || error.message || "Failed to delete order."
+      );
     }
   };
 
@@ -375,94 +126,126 @@ const MyOrders = () => {
   }
 
   return (
-    <>
+    <div className="items-center pt-8 bg-white text-black dark:bg-black dark:text-white min-h-screen">
       <Navbar />
-      <div className="flex flex-col justify-between mt-8 px-6 md:px-16 lg:px-32 py-6 min-h-screen">
+      <main className="flex flex-col justify-between mt-8 px-6 md:px-16 lg:px-32 py-6 min-h-screen">
         <div className="space-y-5">
-          <h2 className="text-lg font-medium mt-6">My Orders</h2>
+          <h2 className="text-lg font-medium mt-6 text-black dark:text-white">
+            My Orders
+          </h2>
           {loading ? (
             <Loading />
+          ) : orders.length === 0 ? (
+            <p className="text-center text-gray-500 dark:text-gray-400">
+              You have no orders yet.
+            </p>
           ) : (
-            <div className="max-w-5xl border-t border-gray-300 text-sm">
-              {orders.map((order, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col md:flex-row gap-5 justify-between p-5 border-b border-gray-300"
-                >
-                  <div className="flex-1 flex gap-5 max-w-80">
-                    <Image
-                      className="max-w-16 max-h-16 object-cover"
-                      src={assets.box_icon}
-                      alt="box_icon"
-                      width={64}
-                      height={64}
-                    />
-                    <p className="flex flex-col gap-3">
-                      <span className="font-medium text-base">
-                        {order.items
-                          .map(
-                            (item) =>
-                              `${item.product.name} x ${item.quantity}`
-                          )
-                          .join(", ")}
-                      </span>
-                      <span>Items: {order.items.length}</span>
+            <section className="max-w-5xl border-t border-gray-300 dark:border-gray-600 text-sm text-black dark:text-white">
+              {orders.map((order, index) => {
+                const method = paymentMethods.find(
+                  (m) => m.id === order.paymentMethod
+                );
+
+                return (
+                  <div
+                    key={order._id || index}
+                    className="flex flex-col md:flex-row gap-5 justify-between p-5 border-b border-gray-300 dark:border-gray-600"
+                  >
+                    {/* Items info */}
+                    <div className="flex-1 flex gap-5 max-w-80">
+                      <Image
+                        className="max-w-16 max-h-16 object-cover"
+                        src={assets.box_icon}
+                        alt="box_icon"
+                        width={64}
+                        height={64}
+                        priority={index === 0} // prioritize first image load
+                      />
+                      <p className="flex flex-col gap-3">
+                        <span className="font-medium text-base text-black dark:text-white">
+                          {order.items
+                            .map((item) => `${item.product.name} x ${item.quantity}`)
+                            .join(", ")}
+                        </span>
+                        <span className="text-black dark:text-white">
+                          Items: {order.items.length}
+                        </span>
+                      </p>
+                    </div>
+
+                    {/* Shipping address */}
+                    <div>
+                      <address className="not-italic text-black dark:text-white">
+                        <p>
+                          <span className="font-medium">{order.address.fullName}</span>
+                          <br />
+                          <span>{order.address.area}</span>
+                          <br />
+                          <span>
+                            {order.address.city}, {order.address.state}
+                          </span>
+                          <br />
+                          <span>{order.address.phoneNumber}</span>
+                        </p>
+                      </address>
+                    </div>
+
+                    {/* Amount */}
+                    <p className="font-medium my-auto whitespace-nowrap text-black dark:text-white">
+                      {currency}
+                      {order.amount.toFixed(2)}
                     </p>
-                  </div>
-                  <div>
-                    <p>
-                      <span className="font-medium">
-                        {order.address.fullName}
-                      </span>
-                      <br />
-                      <span>{order.address.area}</span>
-                      <br />
-                      <span>
-                        {order.address.city}, {order.address.state}
-                      </span>
-                      <br />
-                      <span>{order.address.phoneNumber}</span>
-                    </p>
-                  </div>
-                  <p className="font-medium my-auto">
-                    {currency}
-                    {order.amount}
-                  </p>
-                  <div>
-                    <p className="flex flex-col">
-                      <span>Method: COD</span>
-                      <span>
-                        Date: {new Date(order.date).toLocaleDateString()}
-                      </span>
-                      <span>Payment: Pending</span>
-                      <span>Status: {order.status}</span>
-                    </p>
-                    {order.status !== "Delivered" &&
-                      order.status !== "Cancelled" && (
+
+                    {/* Order details and actions */}
+                    <div>
+                      <p className="flex flex-col text-black dark:text-white">
+                        {method && (
+                          <>
+                          <p className="text-sm text-gray-600">
+                            Payment Method:{" "}
+                            <span className="font-semibold">{method.label}</span>
+                             {/* – Fee:{" "}
+                            {(method.fee * 100).toFixed(1)}% */}
+                          </p>
+                          <p className="text-sm text-gray-600">
+                            Order ID: <span className="font-semibold">{order.orderId || order._id}</span>
+                          </p>
+                          </>
+                        )}
+                        <span className="text-sm text-gray-600">
+                          Date: <span className="font-semibold">{dayjs(order.date).format("DD/MM/YYYY")} • {dayjs(order.date).fromNow()}</span>
+                        </span>
+                        <span className="text-sm text-gray-600">
+                          Payment Status: <span className="font-semibold">{order.paymentStatus || "Pending"}</span>
+                        </span>
+                        <span className="text-sm text-gray-600"> Order Status: <span className="font-semibold">{order.status}</span></span>
+                      </p>
+                      {order.status !== "Delivered" && order.status !== "Cancelled" && (
                         <button
                           onClick={() => handleCancelOrder(order._id)}
-                          className="mt-2 px-3 py-1 text-sm bg-red-500 text-white rounded"
+                          className="mt-2 px-3 py-1 text-sm bg-red-600 hover:bg-red-700 transition text-white rounded"
                         >
                           Cancel Order
                         </button>
                       )}
-                    {order.status === "Cancelled" && (
-                      <button
-                        onClick={() => handleDeleteOrder(order._id)}
-                        className="mt-2 px-3 py-1 text-sm bg-gray-700 text-white rounded"
-                      >
-                        Delete Order
-                      </button>
-                    )}
+                      {order.status === "Cancelled" && (
+                        <button
+                          onClick={() => handleDeleteOrder(order._id)}
+                          className="mt-2 px-3 py-1 text-sm bg-gray-700 hover:bg-gray-800 transition text-white rounded"
+                        >
+                          Delete Order
+                        </button>
+                      )}
+                    </div>
                   </div>
-                </div>
-              ))}
-            </div>
+                );
+              })}
+            </section>
           )}
         </div>
-      </div>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
