3d49623ec32b80a865761ed3ffd19e5e0221a923
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-06-19 13:48:51 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-    _id : { type : String, required : true},
-    name : { type : String, required : true},
-    email : { type : String, required : true, unique : true},
-    imageUrl : { type : String, required : true},
-    cartItems : { type : Object, default : {} }
-    }, 
-    { 
-        minimize : false 
-    }
-)
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+    welcomeSent: { type: Boolean, default: false }, // ✅ correctly placed here
+  },
+  {
+    minimize: false,
+  }
+);
 
-const User = mongoose.models.user || mongoose.model ( 'user', userSchema)
+const User = mongoose.models.user || mongoose.model("user", userSchema);
 
-export default User
+export default User;
