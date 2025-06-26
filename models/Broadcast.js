773ebf39d19b3ca9991076da7ac2b5a17782888a
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-06-26 03:51:28 +0100

    Broadcast.js

---
+++ models/Broadcast.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const BroadcastSchema = new mongoose.Schema(
+  {
+    subject: String,
+    message: String,
+    status: { type: String, default: "pending" },
+    recipients: [
+      {
+        email: String,
+        status: { type: String, enum: ["sent", "failed", "scheduled"], default: "scheduled" },
+        error: String,
+      },
+    ],
+    scheduledFor: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Broadcast || mongoose.model("Broadcast", BroadcastSchema);
+
+
+// export default mongoose.models.Broadcast || mongoose.model("Broadcast", BroadcastSchema);
