0c15772c47b9f98c0e67b5adcd724da162411d0d
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-07-01 00:33:34 +0100

    layout.jsx

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,17 +1,21 @@
 'use client'
+import Footer from '@/components/admin/Footer'
 import Navbar from '@/components/admin/Navbar'
 import Sidebar from '@/components/admin/Sidebar'
 import React from 'react'
 
 const Layout = ({ children }) => {
   return (
+    <>
+    <Navbar />
     <div>
-      <Navbar />
       <div className='flex w-full'>
         <Sidebar />
         {children}
       </div>
     </div>
+    {/* <Footer /> */}
+    </>
   )
 }
 
