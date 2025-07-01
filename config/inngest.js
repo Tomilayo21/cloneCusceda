db2763aef601b6fc6efe8c9274729789f4149a80
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-07-01 23:44:32 +0100

    inngest.js

--- config/inngest.js
+++ config/inngest.js
@@ -4,7 +4,7 @@ import User from "@/models/User";
 import Order from "@/models/Order";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcart-next" });
+export const inngest = new Inngest({ id: "cuscedang" });
 
 // Inngest Function to save  user data to a database
 export const syncUserCreation = inngest.createFunction(
@@ -13,11 +13,13 @@ export const syncUserCreation = inngest.createFunction(
     },
     { event : 'clerk/user.created' },
     async ({event}) => {
-        const {  id, first_name, last_name, email_addresses, image_url} = event.data
+        const {  id, first_name, last_name, email_addresses, image_url, phone_numbers, username } = event.data
         const userData = {
             _id : id,
             email : email_addresses[0].email_address,
+            phoneNumber : phone_numbers[0].phone_number,
             name : first_name + ' ' + last_name,
+            username : username,
             imageUrl : image_url
         }
         await connectDB()
@@ -39,7 +41,9 @@ export const syncUserUpdation = inngest.createFunction (
         const userData = {
             _id : id,
             email : email_addresses[0].email_address,
-            name : first_name + ' ' + last_name,
+            phoneNumber : phone_numbers[0].phone_number,
+            name : first_name + ' ' + last_name,            
+            username : username,
             imageUrl : image_url
         }
         await connectDB()
@@ -76,19 +80,21 @@ export const createUserOrder = inngest.createFunction(
         event : 'order/created'
     },
     async ({ events }) => {
-        
-        const orders = events.map((event) => {
-            return {
-                userId : event.data.userId,
-                items : event.data.items,
-                amount : event.data.amount,
-                address : event.data.address,
-                date : event.data.date
-            }
-        })
+        const orders = events.map((event) => ({
+            userId: event.data.userId,
+            items: event.data.items,
+            amount: event.data.amount,
+            address: event.data.address,
+            date: event.data.date,
+            paymentMethod: event.data.paymentMethod,
+            status: event.data.status || "Pending",
+            proofOfPaymentUrl : event.data.proofOfPaymentUrl,
+            orderId : event.data.orderId,
+        }));
 
-        await connectDB()
-        await Order.insertMany(orders)
+        await connectDB();
+        await Order.insertMany(orders);
+
 
         return { success: true, processed: orders.length };
     }
