bea4178718368ac6fbd87f642a01a4081430f484
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-07-01 23:45:10 +0100

    User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,10 @@ const userSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
     name: { type: String, required: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     imageUrl: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
     cartItems: { type: Object, default: {} },
     welcomeSent: { type: Boolean, default: false }, // ✅ correctly placed here
   },
