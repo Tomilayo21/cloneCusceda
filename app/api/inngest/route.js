9fdd89a51829c4bab07df3e3b0ec70eeaf7e56b0
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-07-12 15:09:04 +0100

    route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,12 @@
 import { serve } from "inngest/next";
-import { createUserOrder, inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { 
+  createUserOrder, 
+  inngest, 
+  syncUserCreation, 
+  syncUserDeletion, 
+  syncUserUpdation, 
+  scheduledBroadcastSender
+} from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
