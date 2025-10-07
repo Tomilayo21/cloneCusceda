ed734d872cdeb29aa5994fb169a0046d86a0eeef
Tomilayo21 <121814988+Tomilayo21@users.noreply.github.com>
2025-10-07 18:29:17 +0100

    page.tsx

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -4,8 +4,8 @@ import RegUsers from "@/components/admin/RegUsers";
 
 const Users = () => {
   return (
-      <div className="flex-1 h-screen overflow-scroll flex flex-col justify-between text-sm">
-          <RegUsers />
+      <div className="">
+        <RegUsers />
       </div>
   );
 };
